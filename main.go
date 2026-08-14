c9c3405b13417a9ad3d1e50a93f18c1e94af4edd
Egor (P0nchiko) <egorsvidko9@gmail.com>
2026-08-14 21:31:49 +0300

    Fix all bags. Project is over

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
-var list_of_users []string //Список всех игроков
-var reader *bufio.Reader   // Глобальный reader
+var listOfUsers []string //Список всех игроков
+var reader *bufio.Reader // Глобальный reader
 
-const MAX_PLAYER_IN_MATCH = 4 // Максимальное колисество игроков на одном сервере
+const maxPlayersPerMatch = 4 // Максимальное колисество игроков на одном сервере
 
-// Читает число из stdin.
-func get_ans_from_user(reader *bufio.Reader) string {
-	fmt.Print("Команда: ")
+// Читает строку из stdin и возвращает её без пробелов по краям.
+func readLine(reader *bufio.Reader, prompt string) string {
+	fmt.Print(prompt)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
@@ -24,54 +24,60 @@ func get_ans_from_user(reader *bufio.Reader) string {
 	return input
 }
 
-// функция new_user добавляет нового игрока в слайс всех игроков на сервере
-func new_user() {
-	fmt.Print("Введите имя игрока: ")
-	user_name := get_ans_from_user(reader)
-	list_of_users = append(list_of_users, user_name)
-	fmt.Printf("Игрок '%s' добавлен (всего: %d)\n", user_name, len(list_of_users))
+// функция newUser добавляет нового игрока в слайс всех игроков на сервере
+func newUser(name string) {
+	listOfUsers = append(listOfUsers, name)
+	fmt.Printf("Игрок '%s' добавлен (всего: %d)\n", name, len(listOfUsers))
 }
 
-// функция all_users выводит всех игроков на сервере
-func all_users() {
-	size_of_list := len(list_of_users)
+// функция allUsers выводит всех игроков на сервере
+func allUsers() {
+	sizeOfList := len(listOfUsers)
 
-	if size_of_list == 0 {
+	if sizeOfList == 0 {
 		fmt.Println("На сервере нет игроков")
 		return
 	}
-	for i := 0; i < size_of_list; i++ {
-		fmt.Println(list_of_users[i])
+	for _, name := range listOfUsers {
+		fmt.Println(name)
 	}
 }
 
-// функция new_match создаёт новый матч если на сервере больше или равно 4-м игроккам,
+// функция newMatch создаёт новый матч если на сервере больше или равно 4-м игроккам,
 // в противном случае выводит ошибку
-func new_match() {
-	if len(list_of_users) < MAX_PLAYER_IN_MATCH {
-		fmt.Printf("Ошибка!!! На сервере меньше %d-х игроков. Матч невозмодно начать", MAX_PLAYER_IN_MATCH)
-	} else {
-		list_of_users = list_of_users[MAX_PLAYER_IN_MATCH:]
-		fmt.Println("Матч успешно запущен")
+func newMatch() {
+	if len(listOfUsers) < maxPlayersPerMatch {
+		fmt.Printf("Ошибка: нужно не менее %d игроков. Сейчас на сервере: %d игроков\n", maxPlayersPerMatch, len(listOfUsers))
+		return
 	}
+	matchPlayers := listOfUsers[:maxPlayersPerMatch]
+	fmt.Println("Матч начинается! Игроки: ")
+	for i, name := range matchPlayers {
+		fmt.Printf("  %d. %s\n", i+1, name)
+	}
+	listOfUsers = listOfUsers[maxPlayersPerMatch:]
+	fmt.Printf("В очереди осталось: %d игроков\n", len(listOfUsers))
 }
 
 func main() {
 	reader = bufio.NewReader(os.Stdin)
-	list_of_users = []string{}
+	listOfUsers = []string{}
 	for {
-		ans_user := get_ans_from_user(reader)
-		if ans_user == "quit" {
+		ansUser := readLine(reader, "Команда: ")
+		ansUser = strings.ToLower(ansUser)
+		switch ansUser {
+		case "quit":
 			fmt.Println("Выход")
-			break
+			return
+		case "add":
+			name := readLine(reader, "Введите имя нового игрока: ")
+			newUser(name)
+		case "list":
+			allUsers()
+		case "match":
+			newMatch()
+		default:
+			fmt.Printf("Неизвестная команда: %q. Доступные: add, list, match, quit\n", ansUser)
 		}
-		if ans_user == "ADD" {
-			new_user()
-		} else if ans_user == "list" {
-			all_users()
-		} else {
-			new_match()
-		}
-
 	}
 }
