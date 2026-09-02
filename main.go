39566d36cc9fa2361675d1f7eef751b7c789039d
Egor (P0nchiko) <egorsvidko9@gmail.com>
2026-09-02 21:07:38 +0300

    Fix all bags and finish project

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 )
 
-var listOfUsers []string //Список всех игроков
-var reader *bufio.Reader // Глобальный reader
-
-const maxPlayersPerMatch = 4 // Максимальное колисество игроков на одном сервере
+type Server struct {
+	players     []string
+	maxPerMatch int
+}
 
 // Читает строку из stdin и возвращает её без пробелов по краям.
 func readLine(reader *bufio.Reader, prompt string) string {
@@ -24,44 +24,63 @@ func readLine(reader *bufio.Reader, prompt string) string {
 	return input
 }
 
-// функция newUser добавляет нового игрока в слайс всех игроков на сервере
-func newUser(name string) {
-	listOfUsers = append(listOfUsers, name)
-	fmt.Printf("Игрок '%s' добавлен (всего: %d)\n", name, len(listOfUsers))
+// AddUser добавляет игрока в очередь.
+func (s *Server) AddUser(name string) {
+	s.players = append(s.players, name)
+	fmt.Printf("Игрок '%s' добавлен (всего: %d)\n", name, len(s.players))
 }
 
-// функция allUsers выводит всех игроков на сервере
-func allUsers() {
-	sizeOfList := len(listOfUsers)
+// функция AllUsers выводит всех игроков на сервере
+func (s *Server) AllUsers() {
 
-	if sizeOfList == 0 {
+	if len(s.players) == 0 {
 		fmt.Println("На сервере нет игроков")
 		return
 	}
-	for _, name := range listOfUsers {
-		fmt.Println(name)
+	for i, name := range s.players {
+		fmt.Printf("  %d. %s\n", i+1, name)
 	}
 }
 
-// функция newMatch создаёт новый матч если на сервере больше или равно 4-м игроккам,
-// в противном случае выводит ошибку
-func newMatch() {
-	if len(listOfUsers) < maxPlayersPerMatch {
-		fmt.Printf("Ошибка: нужно не менее %d игроков. Сейчас на сервере: %d игроков\n", maxPlayersPerMatch, len(listOfUsers))
+// NewMatch собирает матч из первых maxPerMatch игроков.
+func (s *Server) NewMatch() {
+	if len(s.players) < s.maxPerMatch {
+		fmt.Printf("Ошибка: нужно не менее %d игроков. Сейчас на сервере: %d игроков\n", s.maxPerMatch, len(s.players))
 		return
 	}
-	matchPlayers := listOfUsers[:maxPlayersPerMatch]
+	matchPlayers := s.players[:s.maxPerMatch]
 	fmt.Println("Матч начинается! Игроки: ")
 	for i, name := range matchPlayers {
 		fmt.Printf("  %d. %s\n", i+1, name)
 	}
-	listOfUsers = listOfUsers[maxPlayersPerMatch:]
-	fmt.Printf("В очереди осталось: %d игроков\n", len(listOfUsers))
+	s.players = s.players[s.maxPerMatch:]
+	fmt.Printf("В очереди осталось: %d игроков\n", len(s.players))
+}
+
+// Stats выводит количество игроков в очереди.
+func (s *Server) Stats() {
+	fmt.Printf("Количество игроков: %d человек \n", len(s.players))
+}
+
+// Remove удаляет игрока из очереди по имени.
+func (s *Server) Remove(name string) {
+	for i, player := range s.players {
+		if player == name {
+			s.players = append(s.players[:i], s.players[i+1:]...)
+			fmt.Printf("Игрок %q удалён\n", name)
+			return
+		}
+	}
+	fmt.Printf("Игрок %q не найден\n", name)
 }
 
 func main() {
-	reader = bufio.NewReader(os.Stdin)
-	listOfUsers = []string{}
+	reader := bufio.NewReader(os.Stdin)
+
+	server := Server{
+		players:     []string{},
+		maxPerMatch: 4,
+	}
 	for {
 		ansUser := readLine(reader, "Команда: ")
 		ansUser = strings.ToLower(ansUser)
@@ -70,14 +89,19 @@ func main() {
 			fmt.Println("Выход")
 			return
 		case "add":
-			name := readLine(reader, "Введите имя нового игрока: ")
-			newUser(name)
+			server.AddUser(name)
 		case "list":
-			allUsers()
+			server.AllUsers()
 		case "match":
-			newMatch()
+			server.NewMatch()
+		case "stats":
+			server.Stats()
+		case "remove":
+			name := readLine(reader, "Имя человека, которого надо удалить: ")
+			server.Remove(name)
 		default:
-			fmt.Printf("Неизвестная команда: %q. Доступные: add, list, match, quit\n", ansUser)
+			fmt.Printf("Неизвестная команда: %q. Доступные: add, list, match, quit, stats, remove\n", ansUser)
 		}
 	}
 }
+
